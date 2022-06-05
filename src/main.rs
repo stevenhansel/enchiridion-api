66ec34ffe1319346250402d1db0a3e3895355e6d
stevenhansel <stevenhanselgo@gmail.com>
2022-06-05 15:13:18 +0700

    feat: add initial health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use enchiridion_api::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", 8080))
+        .expect(format!("Failed to bind port {}", 8080).as_str());
+
+    run(listener)?.await
 }
