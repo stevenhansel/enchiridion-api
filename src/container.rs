b84c35c3f4732bf71189eb7b58df41972ee0ecbf
stevenhansel <stevenhanselgo@gmail.com>
2022-06-19 20:39:38 +0700

    feat: add initial register logic

--- src/container.rs
+++ src/container.rs
@@ -1,17 +1,18 @@
 use std::sync::Arc;
 
+use argon2::{
+    password_hash::{PasswordHash, PasswordHasher},
+    Argon2,
+};
 use async_trait::async_trait;
-use serde::Serialize;
 use shaku::{module, Component, Interface};
 use sqlx::{Pool, Postgres};
 
-#[derive(Serialize)]
-#[serde(rename_all = "camelCase")]
 pub struct RegisterParams {
-    name: String,
-    email: String,
-    password: String,
-    registration_reason: String,
+    pub name: String,
+    pub email: String,
+    pub password: String,
+    pub reason: String,
 }
 
 pub struct InsertUserParams<'a> {
@@ -21,9 +22,18 @@ pub struct InsertUserParams<'a> {
     registration_reason: String,
 }
 
+pub struct User {
+    id: i32,
+    name: String,
+    email: String,
+    password: Vec<u8>,
+    registration_reason: String,
+}
+
 #[async_trait]
 pub trait UserRepositoryInterface: Interface {
-    async fn create<'a>(&self, params: &InsertUserParams<'a>) -> Result<i32, sqlx::Error>;
+    async fn create<'a>(&self, params: &'a InsertUserParams) -> Result<i32, sqlx::Error>;
+    async fn find_one_by_id(&self, id: i32) -> Result<User, sqlx::Error>;
 }
 
 #[derive(Component)]
@@ -32,15 +42,9 @@ pub struct UserRepository {
     _db: Pool<Postgres>,
 }
 
-impl UserRepository {
-    pub fn new(db: Pool<Postgres>) -> UserRepository {
-        UserRepository { _db: db }
-    }
-}
-
 #[async_trait]
 impl UserRepositoryInterface for UserRepository {
-    async fn create<'a>(&self, params: &InsertUserParams<'a>) -> Result<i32, sqlx::Error> {
+    async fn create<'a>(&self, params: &'a InsertUserParams) -> Result<i32, sqlx::Error> {
         let result = sqlx::query!(
             r#"
             insert into "user" (name, email, password, registration_reason)
@@ -57,11 +61,26 @@ impl UserRepositoryInterface for UserRepository {
 
         Ok(result.id)
     }
+
+    async fn find_one_by_id(&self, id: i32) -> Result<User, sqlx::Error> {
+        let user = sqlx::query_as!(
+            User,
+            r#"
+            select id, name, email, password, registration_reason from "user"
+            where id = $1
+            "#,
+            id
+        )
+        .fetch_one(&self._db)
+        .await?;
+
+        Ok(user)
+    }
 }
 
 #[async_trait]
 pub trait AuthServiceInterface: Interface {
-    async fn register<'a>(&self, params: &InsertUserParams<'a>) -> Result<bool, String>;
+    async fn register(&self, params: RegisterParams) -> Result<User, String>;
 }
 
 #[derive(Component)]
@@ -71,24 +90,34 @@ pub struct AuthService {
     _user_repository: Arc<dyn UserRepositoryInterface>,
 }
 
-impl AuthService {
-    pub fn new(user_repository: Arc<dyn UserRepositoryInterface>) -> AuthService {
-        AuthService {
-            _user_repository: user_repository,
-        }
-    }
-}
-
 #[async_trait]
 impl AuthServiceInterface for AuthService {
-    async fn register<'a>(&self, params: &InsertUserParams<'a>) -> Result<bool, String> {
-        let user_id = match self._user_repository.create(params).await {
+    async fn register(&self, params: RegisterParams) -> Result<User, String> {
+        let hash = match Argon2::default()
+            .hash_password(params.password.as_bytes(), "secretsecretsecretsecret")
+        {
+            Ok(p) => p.serialize(),
+            Err(e) => return Err(e.to_string()),
+        };
+        let password = hash.as_bytes();
+
+        let params = InsertUserParams {
+            name: params.name,
+            email: params.password,
+            registration_reason: params.reason,
+            password,
+        };
+
+        let user_id = match self._user_repository.create(&params).await {
             Ok(id) => id,
             Err(e) => return Err(e.to_string()),
         };
-        println!("user_id: {}", user_id);
+        let user = match self._user_repository.find_one_by_id(user_id).await {
+            Ok(user) => user,
+            Err(e) => return Err(e.to_string()),
+        };
 
-        Ok(true)
+        Ok(user)
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,58 @@ use std::net::TcpListener;
 use std::sync::Arc;
 
 use actix_web::dev::Server;
+use actix_web::web::Json;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+use shaku_actix::Inject;
 use sqlx::{Pool, Postgres};
 
 mod container;
-use container::{Container, UserRepository, UserRepositoryParameters};
+use container::{
+    AuthServiceInterface, Container, RegisterParams, UserRepository, UserRepositoryParameters,
+};
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-async fn register() -> impl Responder {
-    HttpResponse::Ok()
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct RegisterBody {
+    name: String,
+    email: String,
+    password: String,
+    reason: String,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct RegisterResponse {
+    id: i32,
+    name: String,
+    email: String,
+    reason: String,
+}
+
+async fn register(
+    body: Json<RegisterBody>,
+    auth_service: Inject<Container, dyn AuthServiceInterface>,
+) -> impl Responder {
+    let params = RegisterParams {
+        name: body.name.clone(),
+        email: body.email.clone(),
+        password: body.password.clone(),
+        reason: body.reason.clone(),
+    };
+    let user = match auth_service.register(params).await {
+        Ok(user) => user,
+        Err(e) => {
+            println!("{}", e.to_string());
+            return HttpResponse::InternalServerError();
+        }
+    };
+
+    HttpResponse::Created()
 }
 
 pub fn run(listener: TcpListener, pool: Pool<Postgres>) -> Result<Server, std::io::Error> {
@@ -27,6 +67,7 @@ pub fn run(listener: TcpListener, pool: Pool<Postgres>) -> Result<Server, std::i
         App::new()
             .app_data(container.clone())
             .route("/health_check", web::get().to(health_check))
+            .route("/v1/register", web::post().to(register))
     })
     .listen(listener)?
     .run();
