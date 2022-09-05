d75f0f133b4cb68febb4ba8afb8a4f673bc23e8c
stevenhansel <stevenhanselgo@gmail.com>
2022-09-05 23:35:26 +0700

    feat: add seed database for floor and device

---
+++ database/migrations/20220905163059_seed_floor_data.sql
@@ -0,0 +1,14 @@
+-- Add migration script here
+insert into floor (name, building_id)
+values
+  ('Lantai 1', 1),
+  ('Lantai 2', 1),
+  ('Lantai 3', 1),
+
+  ('Lantai 1', 2),
+  ('Lantai 2', 2),
+  ('Lantai 3', 2),
+
+  ('Lantai 1', 3),
+  ('Lantai 2', 3),
+  ('Lantai 3', 3)
