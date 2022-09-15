41e3d0542e14ad236c06fa7cc943a61cc8ac9c95
stevenhansel <stevenhanselgo@gmail.com>
2022-09-15 08:21:24 +0700

    fix: scheduler waiting for approval time query

--- src/features/announcement/repository.rs
+++ src/features/announcement/repository.rs
@@ -653,7 +653,7 @@ impl AnnouncementRepositoryInterface for AnnouncementRepository {
             from "announcement"
             where
                 "status" = 'waiting_for_approval' and
-                "start_date" + '1 day' < $1
+                "start_date" < $1
             "#,
             now,
         )
