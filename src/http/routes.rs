417b7e9859d0a8fba4d43c85c3c0363e61f02541
Steven Hansel <stevenhanselgo@gmail.com>
2022-12-23 18:45:32 +0700

    fix: remove actix files for static serving

--- src/http/routes.rs
+++ src/http/routes.rs
@@ -1,6 +1,5 @@
 use std::sync::Arc;
 
-use actix_files;
 use actix_web::{guard, web, Scope};
 
 use crate::features::{
@@ -402,7 +401,4 @@ pub fn socket_routes() -> Scope {
             "/v1/livestream/{device_id}",
             web::get().to(livestream::route::socket_handler),
         )
-}
-pub fn static_routes() -> Scope {
-    web::scope("/static").service(actix_files::Files::new("/", "./static").show_files_listing())
 }
